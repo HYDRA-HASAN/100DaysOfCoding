daeca6d8015e80fe06385640b8df307ab611e063
Hassan Yaseen Siddiqui <hassanyaseen615@gmail.com>
2024-03-26 00:15:22 +0500

    New

---
+++ day18/54.Making Flexible Object Keys.js
@@ -0,0 +1,12 @@
+"use strict";
+// A way to make a flexible list
+function createObjectWithDynamicKey(key, value) {
+    let dynamicObject = {};
+    // Setting up a section in the list with a changeable name
+    dynamicObject[key] = value;
+    return dynamicObject;
+}
+// Using the flexible list setup for a user's preference
+let userPreference = createObjectWithDynamicKey("theme", "dark");
+// Showing the user's choice
+console.log(userPreference);
