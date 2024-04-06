488e4958411bc0954fe7c3bc178dcab62822ffd4
Hassan Yaseen Siddiqui <hassanyaseen615@gmail.com>
2024-04-07 01:07:47 +0500

    new

---
+++ day37/111.ts
@@ -0,0 +1,13 @@
+function categorizeAge(age: number): string {
+    if (age < 13) {
+        return "Child";
+    } else if (age >= 13 && age < 18) {
+        return "Teenager";
+    } else {
+        return "Adult";
+    }
+}
+
+const age = 25;
+const ageGroup = categorizeAge(age);
+console.log(`The person is an ${ageGroup}.`);
